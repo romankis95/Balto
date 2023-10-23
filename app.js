0cfc9e20423578e96daca68ef66d0426484e2b44
Roman Kis <roman.kiss.scuola@gmail.com>
2023-10-23 11:11:22 +0200

    Cookies

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const ejs = require('ejs')
  */
 const app = express()
 app.use(helmet());
+app.use(express.cookieParser());
+
+// set a cookie
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    res.cookie('Hint',"UXVhbmRvIHNvcnJpZGkgc2luY2VyYW1lbnRlLCB0aSBtb3JkaSBsYSBsaW5ndWEgdHJhIGkgZGVudGksIGUgbWkgcGlhY2UgdW4gc2FjY28=", { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});
 /**
  * Routes part
  */
