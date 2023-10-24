003d08c52d2ea46fadfd73e5bbdbb1813c5b208d
Roman Kis <roman.kiss.scuola@gmail.com>
2023-10-24 14:51:44 +0200

    im tired

--- public/js/personal.js
+++ public/js/personal.js
@@ -33,12 +33,12 @@ const quizData = [{
     {
         "domanda": "Ciao Piccione!",
         "risposta": "Ciao Balto!",
-        "opzioni": ["Ciao Balto!", "Che schifo un piccione.","Ciao Mario"]
+        "opzioni": ["Ciao Balto!", "Che schifo un piccione.", "Ciao Mario"]
     },
     {
         "domanda": "Roman ripara la lavastoviglie te che sei un informatico.\r\nMa mamma...veramente...",
-        "risposta": "Zitto coglione!",
-        "opzioni": ["Zitto coglione!",""]
+        "risposta": "Zitto brutto coglione!",
+        "opzioni": ["Zitto brutto coglione!", "Va bene chiamo un tecnico.", "E' difficile?"]
     },
     {
         "domanda": "Che dos balotaaaaaaas",
@@ -62,8 +62,8 @@ const quizData = [{
     },
     {
         "domanda": "Andiamo a vivere insieme?",
-        "risposta": "Solo se prima affitti un faro",
-        "opzioni": ["Solo se prima affitti un faro", "Si", "No"]
+        "risposta": "Solo se prima affitti un faro dal pover gianni",
+        "opzioni": ["Solo se prima affitti un faro dal pover gianni", "Si", "No"]
     },
     {
         "domanda": "Secondo te, roman imparerà mai ad abbinare pantaloni e maglia?",
@@ -78,7 +78,7 @@ const quizData = [{
     {
         "domanda": "Devi andare a visitare un paese, con quali criteri scegli la meta?",
         "risposta": "Conflitto in corso, mine e missili",
-        "opzioni": ["Conflitto in corso, mine e missili"]
+        "opzioni": ["Conflitto in corso, mine e missili", "Basta che facciano il cornetto al porto", "Vamos vamos!"]
     },
     {
         "domanda": "Hi do you speak english? No.",
@@ -86,12 +86,12 @@ const quizData = [{
         "opzioni": ["Neither do i", "E voi chi cazzo siete?", "Vi porto il conto"]
     },
     {
-        "domanda": "Su 1000 meme che ricevi al giorno, quanti ti fanno ridere? E quant fanno ridere a roman?",
+        "domanda": "Su 1000 meme che ricevi al giorno, quanti ti fanno ridere? E quanti fanno ridere a roman?",
         "risposta": "1 a me, 1000 a lui",
         "opzioni": ["1 a me, 1000 a lui", "tutti", "alcuni"]
     },
     {
-        "domanda": "Il ragazzo giusto ti porta a:",
+        "domanda": "La giornata perfetta consiste in una di queste tre cose:",
         "risposta": "All'IKEA a prendere uno ǍƏƚƚšŒ",
         "opzioni": ["Fare un aperitivo in terrazza",
             "Cena a lume di candela con tizio che suona il violino", "All'IKEA a prendere uno ǍƏƚƚšŒ"
@@ -114,14 +114,14 @@ const quizData = [{
     },
     {
         "domanda": "Cosa vuoi per natale?",
-        "risposta": "Un gruppo col padre di roman dove ci mandiamo solo selfie del cazzo senza chattare",
-        "opzioni": ["Un gruppo col padre di roman dove ci mandiamo solo selfie del cazzo senza chattare","Un viaggio in giappone", "Una carbonara fatta bene"]
+        "risposta": "Un gruppo col padre di roman dove ci mandiamo solo selfie senza chattare",
+        "opzioni": ["Un gruppo col padre di roman dove ci mandiamo solo selfie senza chattare", "Un viaggio in giappone", "Una carbonara fatta bene"]
     },
     {
-        "domanda": "",
-        "risposta": "",
-        "opzioni": [""]
-    }, 
+        "domanda": "Zigo Zago!",
+        "risposta": "Ah, è quasi arrivato, pipì ed esco!",
+        "opzioni": ["Zugo zogo", "Di cosa parli?", "Ah, è quasi arrivato, pipì ed esco!"]
+    },
     //{
     //     "domanda": "",
     //     "risposta": "",
@@ -152,15 +152,60 @@ const questionText = document.getElementById('question-text');
 const optionsList = document.getElementById('options');
 const heartContainer = document.getElementById('heart-container');
 const questionCountText = document.getElementById('question-count'); // Aggiunto il contatore delle domande
+const backgroundImage = document.getElementById("start-image");
+const startbuttoncontainer = document.getElementById('start-button-container')
+const title = document.getElementById('title')
+const prize = document.getElementById('prize')
+const docquiz = document.getElementById('quiz')
 
+
+// Add the paths to your heart images
+const fullHeartImage = '/images/1.png';
+const emptyHeartImage = '/images/2.png';
+const maxquestions = 10;
+const maxNumber = 10;
 let currentQuestionIndex = 0;
 let score = 0;
 let lives = 3;
 let = questionCount = 0;
+const bark = new Audio('/audio/bark.mp3')
+const audio = new Audio('/audio/bones.mp3');
+let randquesitons = []
+startButton.addEventListener('click', () => {
+    randquesitons = generateUniqueRandomIntegers(quizData.length,10)
+    // Nascondi il pulsante "Start" con animazione
+    startButton.style.display = 'none';
+    // Mostra le vite
+    heartContainer.style.display = 'block';
+    backgroundImage.style.display = 'none';
+    backgroundImage.hidden = true
+    startbuttoncontainer.style.display = 'none';
+    // Avvia il quiz
+    loadQuestion();
+    questionContainer.style.display = 'block';
+    bark.play()
+});
+
+
+// Aggiungi questa funzione per generare un array di indici casuali unici
+function getRandomUniqueIndices(max, count) {
+    const indices = [];
+    while (indices.length < count) {
+        const randomIndex = Math.floor(Math.random() * max);
+        if (!indices.includes(randomIndex)) {
+            indices.push(randomIndex);
+        }
+    }
+    return indices;
+}
 
 function loadQuestion() {
-    if (currentQuestionIndex < quizData.length) {
-        const question = quizData[currentQuestionIndex];
+    if (questionCount < maxquestions) { // Carica solo 10 domande casuali
+        const randomIndices = randquesitons.pop();
+        console.log(randomIndices)
+
+        const question = quizData[randomIndices];
+
         questionText.innerText = question.domanda;
         optionsList.innerHTML = '';
         updateQuestionCount(); // Aggiunto l'aggiornamento del contatore
@@ -172,7 +217,7 @@ function loadQuestion() {
             const button = document.createElement('button');
             button.innerText = option;
             button.className = 'option-button'; // Aggiungi una classe per lo stile dei bottoni
-            button.addEventListener('click', () => checkAnswer(option));
+            button.addEventListener('click', () => checkAnswer(option, randomIndices));
             listItem.appendChild(button);
             optionsContainer.appendChild(listItem);
         });
@@ -183,28 +228,29 @@ function loadQuestion() {
     }
 }
 
+
 function updateQuestionCount() {
     questionCount++;
     // Aggiorna il testo del contatore delle domande con il numero corrente e il totale.
-    questionCountText.innerText = `Domanda ${questionCount} su ${quizData.length}`;
+    questionCountText.innerText = `Domanda ${questionCount} su ${maxquestions}`;
 }
 
-function checkAnswer(selectedOption) {
-    const question = quizData[currentQuestionIndex];
+function checkAnswer(selectedOption, index) {
+    const question = quizData[index]
     if (selectedOption === question.risposta) {
         score++;
     } else {
         lives--; // Riduci una vita per ogni errore
         updateLivesDisplay();
     }
-    if (lives===0){
+    if (lives === 0) {
         showResults();
     }
-    if (currentQuestionIndex === quizData.length - 1) {
+    if (index === quizData.length - 1) {
         // Se l'utente ha risposto a tutte le domande, mostra i risultati
         showResults();
     } else {
-        currentQuestionIndex++;
+        index++;
         loadQuestion();
     }
 }
@@ -227,20 +273,13 @@ function showFailure() {
 }
 
 function resetGame() {
-    questionCount = 0;
-    currentQuestionIndex = 0;
-    score = 0;
-    lives = 3;
-    updateLivesDisplay();
-    resultsContainer.style.display = 'none';
-    loadQuestion();
-    questionContainer.style.display = 'block';
+    window.location.reload();
 }
 
 function showResults() {
     questionContainer.style.display = 'none';
     resultsContainer.style.display = 'block';
-    resultsContainer.innerHTML = `Hai totalizzato ${score} punti su ${quizData.length} domande!<br>`;
+    resultsContainer.innerHTML = `Hai totalizzato ${score} punti su ${maxquestions} domande!<br>`;
 
     // Aggiungi il pulsante di congratulazioni se l'utente ha almeno una vita rimasta
     if (lives > 0) {
@@ -249,7 +288,7 @@ function showResults() {
         congratsButton.id = 'congrats-button';
         congratsButton.addEventListener('click', showPrize);
         resultsContainer.appendChild(congratsButton);
-    }else{
+    } else {
         const noLivesMessage = document.createElement('p');
         noLivesMessage.innerText = 'Mi dispiace, secondo il nostro complicatissimo algoritmo, non sei un vero balto';
         resultsContainer.appendChild(noLivesMessage);
@@ -262,54 +301,73 @@ function showResults() {
     }
 }
 
-function showPrize() {
-    // Nascondi il risultato e il pulsante di congratulazioni
-    //resultsContainer.style.display = 'none';
+const imageSources = Array.from({ length: maxNumber+1 }, (_, i) => `/images/slide (${i + 1}).jpeg`);
 
+ 
+  
+function showPrize() {
+
+    audio.play();
     // Crea il contenitore per lo slideshow
     const slideshowContainer = document.createElement('div');
     slideshowContainer.id = 'slideshow-container';
-    resultsContainer.appendChild(slideshowContainer);
-
+    prize.appendChild(slideshowContainer);
+    docquiz.hidden = true
+    title.hidden = true
     // Aggiungi le tue venti foto al tuo slideshow
-    for (let i = 1; i <= 3; i++) {
+    const randoms = generateUniqueRandomIntegers(36,10)
+    for (let i = 0; i <= randoms.length; i++) {
         const slide = document.createElement('div');
-        slide.innerHTML = `<img src="/images/slide${i}.jpeg" alt="Slide ${i}">`;
+        slide.innerHTML = `<img src="/images/slide (${randoms[i]}).jpeg" alt="Slide ${i}">`;
         slideshowContainer.appendChild(slide);
     }
 
     // Inizializza Slick Slider
-    $(slideshowContainer).slick({
+    const slickSlideshow = $(slideshowContainer).slick({
         infinite: true,
+        lazyLoad: 'anticipated',
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000, // Intervallo tra le diapositive in millisecondi
+        responsive: [{
+            breakpoint: 480, // Imposta il breakpoint per dispositivi con larghezza massima di 480px (tipicamente, iPhone in modalità retrato)
+            settings: {
+                slidesToShow: 1, // Visualizza 1 slide alla volta
+                slidesToScroll: 1, // Scorri 1 slide alla volta
+                centerMode: true, // Centra le immagini
+                centerPadding: "0", // Padding
+                variableWidth: true, // Larghezza variabile per adattarsi allo schermo
+                adaptiveHeight: true, // Altezza adattiva
+            }
+       }]
     });
 
-    // Aggiungi musica al tuo slideshow (assicurati di avere il file audio nel tuo progetto)
-    // const audio = new Audio('your-audio-file.mp3');
-    // audio.play();
+// Aggiungi un gestore di eventi per controllare quando il slideshow è stato inizializzato
+slickSlideshow.on('init', function () {
+  // Questo è il gestore dell'evento 'init' che si attiva quando il slideshow è stato inizializzato
+  console.log('Slideshow inizializzato correttamente');
+});
+
+// Aggiungi un gestore di eventi per controllare quando una diapositiva è cambiata
+slickSlideshow.on('afterChange', function (event, slick, currentSlide) {
+  // Questo è il gestore dell'evento 'afterChange' che si attiva quando una diapositiva è cambiata
+  console.log(`Diapositiva corrente: ${currentSlide}`);
+  // Puoi eseguire controlli qui e forzare la ricarica delle immagini se necessario
+});
+
+
 }
 
-// function updateLivesDisplay() {
-//     heartContainer.innerHTML = '';
-//     for (let i = 0; i < lives; i++) {
-//         const heartIcon = document.createElement('span');
-//         heartIcon.innerHTML = '❤️';
-//         heartContainer.appendChild(heartIcon);
-//     }
-// }
- 
-// Add the paths to your heart images
-const fullHeartImage = '/images/1.png';
-const emptyHeartImage = '/images/2.png';
+
+
 
 function updateLivesDisplay() {
     heartContainer.innerHTML = '';
     for (let i = 0; i < lives; i++) {
         const heartIcon = document.createElement('img');
         heartIcon.src = fullHeartImage; // Display a full heart
+        heartIcon.classList.add('hfull')
         heartIcon.alt = '❤️'; // Alt text for accessibility
         heartContainer.appendChild(heartIcon);
     }
@@ -317,6 +375,7 @@ function updateLivesDisplay() {
     for (let i = 0; i < remainingEmptyHearts; i++) {
         const heartIcon = document.createElement('img');
         heartIcon.src = emptyHeartImage; // Display an empty heart for remaining lives
+        heartIcon.classList.add('hempty')
         heartIcon.alt = '🤍'; // Alt text for accessibility
         heartContainer.appendChild(heartIcon);
     }
@@ -324,13 +383,26 @@ function updateLivesDisplay() {
 
 updateLivesDisplay();
 
-startButton.addEventListener('click', () => {
-    // Nascondi il pulsante "Start" con animazione
-    startButton.style.display = 'none';
-    // Mostra le vite
-    heartContainer.style.display = 'block';
 
-    // Avvia il quiz
-    loadQuestion();
-    questionContainer.style.display = 'block';
-});
+
+
+function generateUniqueRandomIntegers(maxNumber, count) {
+    if (maxNumber < count) {
+      console.error('Max number should be greater than or equal to the count.');
+      return [];
+    }
+  
+    const result = [];
+    for (let i = 0; i < count; i++) {
+      let randomNumber;
+      do {
+        randomNumber = Math.floor(Math.random() * (maxNumber + 1));
+      } while (result.includes(randomNumber));
+      result.push(randomNumber);
+    }
+    
+    console.log(result)
+    return result;
+  }
+
+
